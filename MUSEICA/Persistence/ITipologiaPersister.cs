b28c679335b86a2c002ccffbebe33ed518a6a361
Sallivan80 <nicolas.montesi.92@gmail.com>
2014-10-05 23:00:07 +0200

    Implementazione TipologiaPersister

---
+++ MUSEICA/Persistence/ITipologiaPersister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MUSEICA.Model;
+
+namespace MUSEICA.Persistence
+{
+    public interface ITipologiaPersister
+    {
+        ITipologiaLoader GetLoader(string typeFileLoader);
+
+        ITipologiaSaver GetSaver(string typeFileLoader);
+    }
+
+    public interface ITipologiaLoader
+    {
+        List<ITipologiaSala> LoadTipologie();
+
+    }
+
+    public interface ITipologiaSaver
+    {
+        void SaveUpdateTipologia(ITipologiaSala tipologia);
+
+    }
+}
