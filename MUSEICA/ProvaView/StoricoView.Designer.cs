3850b2043e5dbe56a9f8af8a826cbb5bc504e78e
Francesco Zobel <zobel.francesco@gmail.com>
2014-10-11 18:18:25 +0200

    Commit

--- MUSEICA/ProvaView/StoricoView.Designer.cs
+++ MUSEICA/ProvaView/StoricoView.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,21 +44,34 @@
             this.dataGridView1.Size = new System.Drawing.Size(542, 216);
             this.dataGridView1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Prenotazioni:";
+            // 
             // StoricoView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(567, 261);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "StoricoView";
             this.Text = "Storico Prenotazioni";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MUSEICA/ProvaView/StoricoView.cs
+++ MUSEICA/ProvaView/StoricoView.cs
@@ -16,5 +16,6 @@ namespace MUSEICA.ProvaView
         {
             InitializeComponent();
         }
+
     }
 }
